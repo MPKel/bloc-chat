b126e22c27c5b48d59440d5020b8ef118f6f85c2
Michael <mpkelley08@gmail.com>
2017-11-15 14:32:50 -0500

    added $watch function to fix setting/reading username cookie

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,13 +1,18 @@
 (function() {
-    function HomeCtrl(Room, Message, $cookies) {
+    function HomeCtrl(Room, Message, $cookies, $scope) {
 
       this.rooms = Room.all;
 
       this.activeRoom = null;
 
       this.userName = $cookies.get('blocChatCurrentUser') || null;
+      $scope.$watch(() => $cookies.get('blocChatCurrentUser'), (newValue) => this.userName = newValue);
 
-    
+
+
+
+
+
       this.displayRoom = function (Room){
         this.activeRoom = Room.$id;
         this.messages = Message.getByRoomId(this.activeRoom);
@@ -21,5 +26,5 @@
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', 'Message', '$cookies', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$cookies', '$scope', HomeCtrl]);
 })();
